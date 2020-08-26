5827f89f95671ef25d43198e8c5863a96c6d0130
Oscar Morales <oskralberto14@gmail.com>
2020-08-25 19:31:04 -0600

    Agregando paginas

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser= require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +25,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Bootstrap 4 y librerías necesarias
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Inicio' });
 });
+
+/* GET agregar page. */
+router.get('/agregar', function(req, res, next) {
+  res.render('agregar', { title: 'Agregar' });
+});
+
+/* GET consultar page. */
+router.get('/consultar', function(req, res, next) {
+  res.render('consultar', { title: 'Consultar' });
+});
+
+/* GET grupo page. */
+router.get('/grupo', function(req, res, next) {
+  res.render('grupo', { title: 'Grupo' });
+});
+
 
 module.exports = router;
