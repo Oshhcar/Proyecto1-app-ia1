93e4befd213caf0c86244d4f9cb8d7165caa09da
Oscar Morales <oskralberto14@gmail.com>
2020-08-25 20:28:49 -0600

    Terminando agregar

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+
+/*
+MongoClient.connect("mongodb+srv://wilson:wilson123@cluster0.jgcb4.gcp.mongodb.net/estudiante?retryWrites=true&w=majority", {
+  useUnifiedTopology: true
+}, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+})
+*/
+
+MongoClient.connect("mongodb+srv://wilson:wilson123@cluster0.jgcb4.gcp.mongodb.net/practica1?retryWrites=true&w=majority", { 
+  useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db('practica1');
+    const estudiantesCollection = db.collection('estudiantes');
+
+    /* POST agregar. */
+    router.post('/agregar', function(req, res, next) {
+      estudiantesCollection.insertOne(req.body)
+      .catch(error=>console.error(error));
+
+      res.redirect('/agregar')
+    });
+
+
+  })
+  .catch(console.error)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
